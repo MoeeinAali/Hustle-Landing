f6e13dff35f5dc73cb70b3cf7056c60af3ed6003
Moeein Aali <moeeeinaali@gmail.com>
2025-07-06 20:29:38 +0330

    [FEAT] add motion to hero section

--- src/components/hero/components/hero-texts.component.tsx
+++ src/components/hero/components/hero-texts.component.tsx
@@ -1,25 +1,29 @@
 import type {ReactElement} from "react";
+import {motion} from "motion/react";
+
+import {slideUp} from "../../../utils/animation.ts";
 
 export default function HeroTextsComponent(): ReactElement {
     return (
         <div
             className="space-y-5 flex flex-col justify-center items-center text-center md:text-left py-20 px-10 md:pr-24 md:py-0 md:px-0 md:items-start">
-            <h1
+            <motion.h1
+                {...slideUp(0.2)}
                 className="text-4xl xl:text-5xl font-bold"
             >
                 Unlock a Passion, Side Hustle, or New{" "}
                 <span className="text-gray-400 underline">Profession</span>
-            </h1>
-            <p
-            >
+            </motion.h1>
+            <motion.p {...slideUp(0.4)}>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Dignissimos eligendi mollitia{" "}
-            </p>
-            <button
+            </motion.p>
+            <motion.button
                 className="primary-btn bg-gray-900 hover:bg-primary"
+                {...slideUp(0.6)}
             >
                 More News
-            </button>
+            </motion.button>
         </div>
     );
 }
